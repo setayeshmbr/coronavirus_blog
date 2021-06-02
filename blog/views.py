2bb94e905ac7e5401776ac81fd301a90a24f1a9f
Setayesh Mohammadi <setayeshmbr26@gmail.com>
2021-06-02 12:48:53 +0430

    added redis to project and used it in article detail view

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
 from account.mixins import AuthorAccessMixin
 from account.models import User
 from blog.models import Article, Category
+from django.core.cache import cache
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.conf import settings
 
+CACHE_TTL  =  getattr(settings , 'CACHE_TTL' ,DEFAULT_TIMEOUT )
 
 class ArticleList(ListView) :
     # model = Article
@@ -14,7 +18,18 @@ class ArticleList(ListView) :
 class ArticleDetail(DetailView) :
     def get_object(self, queryset=None) :
         slug = self.kwargs.get('slug')
-        article =get_object_or_404(Article.objects.published(), slug=slug)
+        if cache.get(slug):
+            print("data from cacheeeeeeeeeeee*********")
+            article = cache.get(slug)
+        else:
+            try:
+                article =get_object_or_404(Article.objects.published(), slug=slug)
+                cache.set(slug, article)
+                print("data from databaseeeeeeeeeeeeeeeeeeeee **********")
+            except Article.DoesNotExist:
+                return redirect('/')
+
+
         ip_address = self.request.user.ip_address
         if ip_address not in article.hits.all():
             article.hits.add(ip_address)
@@ -58,35 +73,3 @@ class AuthorList(ListView) :
         context['author'] = author
         return context
 
-
-
-# def home(request):
-#     article_list = Article.objects.published()
-#     paginator = Paginator(article_list , 3)
-#     page = request.GET.get('page')
-#     articles = paginator.get_page(page)
-#     context ={
-#         'articles': articles,
-#     }
-#     return render(request, 'blog/home.html' , context)
-
-
-# def detail (request ,slug):
-#     context = {
-#         'article': get_object_or_404(Article.objects.published(), slug=slug)
-#     }
-#     return render(request, 'blog/article_detail.html', context)
-
-
-# def category(request, slug):
-#     category =  get_object_or_404(Category, slug=slug, status=True)
-#     article_list = category.articles.published()
-#
-#     paginator = Paginator(article_list, 3)
-#     page = request.GET.get('page')
-#     articles = paginator.get_page(page)
-#     context = {
-#         'category' : category,
-#         'articles' : articles
-#     }
-#     return render(request, 'blog/home.html', context)
